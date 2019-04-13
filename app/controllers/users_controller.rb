b909f6d4fffd49fdbc4ab04d37627a29014db7ae
Rio Sanka <rio@RionoMacBook.local>
2019-04-13 21:07:11 +0900

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def index
+    #ホーム
+  end
+
+  def show
+    #ユーザ詳細ページ
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'アカウントを作成しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'アカウントの作成に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    #ユーザプロフィール変更
+  end
+
+  def update
+    #変更の保存
+  end
+
+  def destroy
+    #ユーザ削除
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
