68234c74a673043f89e54b2e2038c72c44861d7c
Rio Sanka <rio@RionoMacBook.local>
2019-04-23 22:14:07 +0900

    pagination

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @microposts = current_user.microposts.order('created_at DESC')
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
       @micropost = current_user.microposts.build
       counts
     end
