b2358d25449c21c80d46a30cc0759745798d81d3
Rio Sanka <threehouse.second@gmail.com>
2019-05-10 10:59:08 +0900

    flash good follow

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -4,12 +4,14 @@ class GoodsController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
     current_user.good(micropost)
+    flash[:success] = "いいねをしました"
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.ungood(micropost)
+    flash[:danger] = "いいねを取り消しました"
     redirect_back(fallback_location: root_path)
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:follow_id])
     current_user.follow(user)
+    flash[:success] = "フォローしました"
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
+    flash[:danger] = "フォローを解除しました"
     redirect_back(fallback_location: root_path)
   end
 end
