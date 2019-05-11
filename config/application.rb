1a6fd953545d9ca5cbd251b1382187ffd72d63e2
Rio Sanka <threehouse.second@gmail.com>
2019-05-11 16:58:19 +0900

    yml change

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module FirstApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
